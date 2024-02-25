7b6ec8148eba6063a8587cee187c8c18db752020
Tester_Nik <katruk.nikita5@gmail.com>
2024-02-25 11:37:53 +0200

    HM2_JS

---
+++ js_basic_task1/task1.js
@@ -0,0 +1,5 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('This is cool!'));
+console.log(chalk.green('This is awesome!'));
+console.log(chalk.red('This is amazing!'));
---
+++ js_basic_task1/task2.js
@@ -0,0 +1,7 @@
+let sampleString = 'This is text';
+let sampleNum = 55;
+let sampleTrueOrFalse = true;
+
+console.log(sampleString);
+console.log(sampleNum);
+console.log(sampleTrueOrFalse);
