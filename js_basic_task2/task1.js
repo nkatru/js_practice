79a3b6d3de435dce6eb8f09f6cd1f3ec7166fce1
Tester_Nik <katruk.nikita5@gmail.com>
2024-02-29 23:50:03 +0200

    HM_JS_3-fixes added

--- js_basic_task2/task1.js
+++ js_basic_task2/task1.js
@@ -1,37 +1,36 @@
 function revenueCount(baseRevenue) {
-let percentSymbol = '%';
-let revenueAfterTaxes;
-let percentValue;
+    let percentSymbol = '%';
+    let revenueAfterTaxes;
+    let percentValue;
 
-if (baseRevenue < 10000 && baseRevenue > 0) {
-    percentValue = 5;
-    revenueAfterTaxes = baseRevenue * percentValue/100;
-    console.log(`Base revenue = ${baseRevenue}. Taxes percent = ${percentValue}${percentSymbol}. Revenue after taxes = ${revenueAfterTaxes}.`);
-}
-else if (baseRevenue <= 50000 && baseRevenue >= 10000) {
-    percentValue = 10;
-    revenueAfterTaxes = baseRevenue * percentValue/100;
-    console.log(`Base revenue = ${baseRevenue}. Taxes percent = ${percentValue}${percentSymbol}. Revenue after taxes = ${revenueAfterTaxes}.`);
-}
-else if (baseRevenue <= 100000 && baseRevenue > 50000) {
-    percentValue = 15;
-    revenueAfterTaxes = baseRevenue * percentValue/100;
-    console.log(`Base revenue = ${baseRevenue}. Taxes percent = ${percentValue}${percentSymbol}. Revenue after taxes = ${revenueAfterTaxes}.`);
-}
-else if (baseRevenue <= 200000 && baseRevenue > 100000) {
-    percentValue = 20;
-    revenueAfterTaxes = baseRevenue * percentValue/100;
-    console.log(`Base revenue = ${baseRevenue}. Taxes percent = ${percentValue}${percentSymbol}. Revenue after taxes = ${revenueAfterTaxes}.`);
-}
-else if (baseRevenue > 200000 ) {
-    percentValue = 25;
-    revenueAfterTaxes = baseRevenue * percentValue/100;
-    console.log(`Base revenue = ${baseRevenue}. Taxes percent = ${percentValue}${percentSymbol}. Revenue after taxes = ${revenueAfterTaxes}.`);
-}
-else {
-    console.log(`Invalid value.`)
-}
-}
+    if (baseRevenue < 10000 && baseRevenue > 0) {
+        percentValue = 5;
+        revenueAfterTaxes = baseRevenue - (baseRevenue * percentValue / 100);
+    }
+    else if (baseRevenue <= 50000 && baseRevenue >= 10000) {
+        percentValue = 10;
+        revenueAfterTaxes = baseRevenue - (baseRevenue * percentValue / 100);
+    }
+    else if (baseRevenue <= 100000 && baseRevenue > 50000) {
+        percentValue = 15;
+        revenueAfterTaxes = baseRevenue - (baseRevenue * percentValue / 100);
+    }
+    else if (baseRevenue <= 200000 && baseRevenue > 100000) {
+        percentValue = 20;
+        revenueAfterTaxes = baseRevenue - (baseRevenue * percentValue / 100);
+    }
+    else if (baseRevenue > 200000) {
+        percentValue = 25;
+        revenueAfterTaxes = baseRevenue - (baseRevenue * percentValue / 100);
+    }
+    else {
+        console.log(`Invalid value.`);
+    }
+    console.log(`Base revenue = ${baseRevenue}. 
+Taxes percent = ${percentValue}${percentSymbol}.
+Revenue after taxes = ${revenueAfterTaxes}.`);
+
+};
 
 
-revenueCount(1000);
+revenueCount(50000);
