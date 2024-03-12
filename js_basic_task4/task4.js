1356e97e7be1959238141eb50977ac7e0ade7c72
Tester_Nik <katruk.nikita5@gmail.com>
2024-03-12 15:49:42 +0200

    HM_changes

--- js_basic_task4/task4.js
+++ js_basic_task4/task4.js
@@ -1,9 +1,7 @@
 const names = ["John", "Fernando", "Alexandra", "Anatolii", "Arina", "Elon"];
 let nameToFind;
 
-for (let nme of names) {
-  if (nme.length > 6) {
-    console.log(nme);
-    break;
-  }
-}
+nameToFind = names.find((value) => {
+  return value.length > 5;
+});
+console.log(nameToFind);
--- js_basic_task4/task6.js
+++ js_basic_task4/task6.js
@@ -4,5 +4,5 @@ let newEmail;
 
 newArr = email.split("@", 2);
 newArr.splice(1, 1, "changed.com");
-newEmail = newArr.join("@")
-;console.log(newEmail);
+newEmail = newArr.join("@");
+console.log(newEmail);
